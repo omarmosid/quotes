e0bfe591582a29263a77e6c0fe990d4d9c9a90bc
Omar Mohammad <omarmo@cloudflare.com>
2023-12-10 12:19:17 +0000

    Add html page

--- src/index.ts
+++ src/index.ts
@@ -32,24 +32,62 @@ export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const route = new URL(request.url).pathname;
 		console.log('route', route);
-		switch (route) {
-			case '/all':
-				const allResponse = JSON.stringify(quotes, null, 2);
-				return new Response(allResponse, {
-					headers: {
-						'content-type': 'application/json;charset=UTF-8',
-					},
-				});
-			case '/random':
-				const randomQuote = getRandomFromArray(quotes);
-				const randomQuoteResponse = JSON.stringify(randomQuote, null, 2);
-				return new Response(randomQuoteResponse, {
-					headers: {
-						'content-type': 'application/json;charset=UTF-8',
-					},
-				});
-			default:
-				return new Response('Hello world');
+
+		if (route === '/api/all') {
+			const allResponse = JSON.stringify(quotes, null, 2);
+			return new Response(allResponse, {
+				headers: {
+					'content-type': 'application/json;charset=UTF-8',
+				},
+			});
+		} else if (route === '/api/random') {
+			const randomQuote = getRandomFromArray(quotes);
+			const randomQuoteResponse = JSON.stringify(randomQuote, null, 2);
+			return new Response(randomQuoteResponse, {
+				headers: {
+					'content-type': 'application/json;charset=UTF-8',
+				},
+			});
+		} else {
+			const randomQuote = getRandomFromArray(quotes);
+			const html = `<html lang="en">
+			<head>
+				<meta charset="UTF-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<meta name="color-scheme" content="dark light">
+				<title>Omars favourites quotes!</title>
+				<style>
+					body {
+						padding: 4em;
+						display: flex;
+						justify-content: center;
+						align-items: center;
+						flex-wrap: wrap;
+					}
+					h1 {
+						font-size: 1.6em;
+						max-width: 20em;
+						width: 100%;
+					}
+					p.sub {
+						display: inline-block;
+						width: 100%;
+						color: rgba(255, 255, 255, 50%);
+					}
+				</style>
+			</head>
+			<body>
+				<div class="container">
+					<h1>${randomQuote?.text}</h1>
+					<p class="sub">- ${randomQuote?.author}</p>
+				</div>
+			</body>
+		</html>`;
+			return new Response(html, {
+				headers: {
+					'content-type': 'text/html;charset=UTF-8',
+				},
+			});
 		}
 	},
 };
